0667521ed057adfb6f052f10441838020e78605a
Roshanell Francisco <96213223+Roshanell@users.noreply.github.com>
2023-03-24 12:58:37 -0500

    adding hardcoded data

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -1,4 +1,95 @@
 const Contacts = () => {
+	[
+		{
+			id: 1,
+			first_name: "Alane",
+			last_name: "Ingyon",
+			email: "aingyon0@prnewswire.com",
+			phone: "219-337-8773",
+			notes:
+				"nam nulla integer pede justo lacinia eget tincidunt eget tempus vel pede",
+		},
+		{
+			id: 2,
+			first_name: "Ernie",
+			last_name: "Phelps",
+			email: "ephelps1@networkadvertising.org",
+			phone: "213-805-4382",
+			notes:
+				"orci mauris lacinia sapien quis libero nullam sit amet turpis elementum ligula vehicula consequat morbi a ipsum integer",
+		},
+		{
+			id: 3,
+			first_name: "Tod",
+			last_name: "Dyball",
+			email: "tdyball2@hubpages.com",
+			phone: "397-823-0356",
+			notes:
+				"venenatis turpis enim blandit mi in porttitor pede justo eu massa donec dapibus duis at",
+		},
+		{
+			id: 4,
+			first_name: "Duffy",
+			last_name: "Buye",
+			email: "dbuye3@pagesperso-orange.fr",
+			phone: "777-326-9657",
+			notes: "nisl nunc rhoncus dui vel sem sed sagittis nam congue",
+		},
+		{
+			id: 5,
+			first_name: "Thurstan",
+			last_name: "Bowden",
+			email: "tbowden4@phpbb.com",
+			phone: "850-511-2880",
+			notes:
+				"posuere cubilia curae mauris viverra diam vitae quam suspendisse potenti nullam porttitor lacus at turpis donec posuere",
+		},
+		{
+			id: 6,
+			first_name: "Minni",
+			last_name: "Avramovich",
+			email: "mavramovich5@usgs.gov",
+			phone: "806-969-9777",
+			notes:
+				"aliquet at feugiat non pretium quis lectus suspendisse potenti in eleifend quam a odio in hac habitasse",
+		},
+		{
+			id: 7,
+			first_name: "Faydra",
+			last_name: "Hanselman",
+			email: "fhanselman6@typepad.com",
+			phone: "875-192-7932",
+			notes:
+				"nisi volutpat eleifend donec ut dolor morbi vel lectus in quam fringilla rhoncus mauris enim leo rhoncus sed vestibulum",
+		},
+		{
+			id: 8,
+			first_name: "Felicdad",
+			last_name: "Viant",
+			email: "fviant7@csmonitor.com",
+			phone: "128-341-3200",
+			notes:
+				"ultricies eu nibh quisque id justo sit amet sapien dignissim vestibulum vestibulum",
+		},
+		{
+			id: 9,
+			first_name: "Elisabetta",
+			last_name: "Lympany",
+			email: "elympany8@w3.org",
+			phone: "887-887-2565",
+			notes:
+				"vehicula condimentum curabitur in libero ut massa volutpat convallis morbi odio odio elementum eu interdum eu tincidunt in leo maecenas",
+		},
+		{
+			id: 10,
+			first_name: "Conway",
+			last_name: "Gutch",
+			email: "cgutch9@irs.gov",
+			phone: "577-285-2333",
+			notes:
+				"eu orci mauris lacinia sapien quis libero nullam sit amet turpis elementum ligula vehicula consequat morbi a ipsum integer a",
+		},
+	];
 	return (
 		<div>
 			<p>I am the Contacts component</p>
